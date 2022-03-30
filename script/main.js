f3e799b4b29698a78264d5792a5642a7613e117f
oebfx <99594672+oebfx@users.noreply.github.com>
2022-03-30 23:41:06 +0300

    Update main.js

--- script/main.js
+++ script/main.js
@@ -2,7 +2,7 @@
 // тут мы вешаем на кнопку МЕНЮ событие которое при клике на кнопку
 // добавляет menu_mobile некий класс, в данном случае это active
 
-document.querySelector('.mobile-menu').addEventListener('click ', () => {
+document.querySelector('.mobile-menu').addEventListener('click touchstart ', () => {
 
     document.querySelector('.nav-items').classList.toggle('active');
 
