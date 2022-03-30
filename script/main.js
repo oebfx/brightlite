ea0ec0e045063a564ace0d98e136dc9439371fbb
oebfx <99594672+oebfx@users.noreply.github.com>
2022-03-30 23:36:08 +0300

    Update main.js

--- script/main.js
+++ script/main.js
@@ -1,12 +1,12 @@
 //меню  
 // тут мы вешаем на кнопку МЕНЮ событие которое при клике на кнопку
 // добавляет menu_mobile некий класс, в данном случае это active
-const mobile-menu = document.querySelector('.mobile-menu')
+const mobile-menu = document.querySelector('.mobile-menu');
 mobile-menu.addEventListener('click', () => {
 
     document.querySelector('.nav-items').classList.toggle('active');
 
-})
+});
 
 
 
