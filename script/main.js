5694f13fad78690114d5e6b071cea168d46dd721
oebfx <99594672+oebfx@users.noreply.github.com>
2022-04-02 21:08:54 +0300

    Add files via upload

--- script/main.js
+++ script/main.js
@@ -1,18 +1,55 @@
-//меню  
+//меню 
+const mobile_menu = document.querySelector('.mobile-menu');
+const menu = document.querySelector('.nav-items');
+const toggleMenu = function () {
+    menu.classList.toggle('active');
+}
 
 
-document.querySelector('.mobile-menu').addEventListener('click', () => {
+mobile_menu.addEventListener('click', function (e) {
+    e.stopPropagation();
+    toggleMenu();
+});
 
-    document.querySelector('.nav-items').classList.toggle('active');
 
-})
+document.addEventListener('click', function (e) {
+    const target = e.target;
+    const its_menu = target == menu || menu.contains(target);
+    const its_btnMenu = target == mobile_menu;
+    const menu_is_active = menu.classList.contains('active');
+
+    if (!its_menu && !its_btnMenu && menu_is_active) {
+        toggleMenu();
+    }
+
+
+});
+
+
 //соцсети
-document.querySelector('.footer-social-mobile').addEventListener('click', () => {
+const footer_social_mobile = document.querySelector('.footer-social-mobile');
+const social = document.querySelector('.footer-social__items');
+const toggleSocial = function () {
+    social.classList.toggle('active');
+    footer_social_mobile.classList.toggle('active');
+}
 
-    document.querySelector('.footer-social__items').classList.toggle('active');
-    document.querySelector('.footer-social-mobile').classList.toggle('active');
 
-})
+footer_social_mobile.addEventListener('click', function (e) {
+    e.stopPropagation();
+    toggleSocial();
+});
+
+document.addEventListener('click', function (e) {
+    const targets = e.target;
+    const its_social = targets == social || social.contains(targets);
+    const its_footer_social = targets == footer_social_mobile;
+    const footer_social_is_active = social.classList.contains('active');
+
+    if (!its_social && !its_footer_social && footer_social_is_active) {
+        toggleSocial();
+    }
+});
 
 
 
@@ -37,7 +74,7 @@ const swiper = new Swiper('.main_slider', {
 
 
 
-    // Navigation arrows
+    // стрелки навигации
     navigation: {
 
         prevEl: '.main_slider_arrow-prev',
